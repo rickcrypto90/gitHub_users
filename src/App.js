ac74462f675b7e547bdfbd1bae1b832920e128fd
Riccardo Maria Pirrello <r.pirrello@thecove.it>
2024-06-20 15:59:45 +0200

    feat: first release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useLayoutEffect, useState } from "react";
+import { CenteredInput, ResultsList } from "./components";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleInputChange = useCallback(
+    (e) => {
+      setQuery(e.target.value);
+    },
+    [setQuery]
+  );
+
+  const handleInputSubmit = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://api.github.com/search/users?q=${query}`
+      );
+      const responseJson = await response.json();
+      setResults(responseJson.items);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }, [query]);
+
+  const handleGetUsers = useCallback(async () => {
+    try {
+      const response = await fetch("https://api.github.com/users");
+      const responseJson = await response.json();
+      setResults(responseJson);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (!results.length) {
+        try {
+          await handleGetUsers();
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      } else {
+        if (!query) {
+          await handleGetUsers();
+        }
+      }
+    })();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <CenteredInput
+        query={query}
+        onInputChange={handleInputChange}
+        onInputSubmit={handleInputSubmit}
+      />
+
+      <ResultsList results={results} />
     </div>
   );
-}
+};
 
 export default App;
